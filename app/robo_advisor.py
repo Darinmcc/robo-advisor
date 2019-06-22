4877876610fe8dd8903a460f4d70295c73f8813f
Darinmcc <50928612+Darinmcc@users.noreply.github.com>
2019-06-22 00:34:54 -0400

    get requests and parse

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,5 +1,41 @@
 # app/robo_advisor.py
 
+import requests # to make requests for https package - need to install request package in virtual environme
+import json #use to convert json string to dictionary #module don't need to install in virtual part of python
+
+#
+# INFO INPUTS
+#
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo" # variable for Url
+
+response = requests.get(request_url) #< response variable - sends get requests, specify the URL for the request - see documentation
+
+#print(type(response)) <data type - class 'requests.models.Response
+
+#print(response.status_code) <200 - successful #status code attribute - https lets us know how successful the request was, and 
+
+#print(response.text) <actual body of response. #string need to convert to dictionary
+
+#print(type(response.text)) <class 'str'
+
+parsed_response = json.loads(response.text) # variable, parse str to dict
+
+breakpoint()
+
+
+
+
+
+#
+#INFO OUTPUTS
+#
+
+
+
+
+
+
+
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
