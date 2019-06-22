c97a18043633f9d180d54833717f0dae99f33975
Darinmcc <50928612+Darinmcc@users.noreply.github.com>
2019-06-22 01:31:32 -0400

    parsed data

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -20,9 +20,10 @@ response = requests.get(request_url) #< response variable - sends get requests, 
 
 parsed_response = json.loads(response.text) # variable, parse str to dict
 
-breakpoint()
+#breakpoint()
 
-
+#parsed_reponse - type() = dict, parsed_reponse.keys() = top keys
+#parsed_response["Meta Data"].keys()
 
 
 
@@ -30,6 +31,7 @@ breakpoint()
 #INFO OUTPUTS
 #
 
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
 
 
@@ -40,9 +42,9 @@ print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print(f"REQUEST AT: {last_refreshed}")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
+print(f"LATEST DAY: {last_refreshed}")
 print("LATEST CLOSE: $100,000.00")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
