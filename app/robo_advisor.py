ca3cde4bd8c0abaf65340fe5ab31d5179e78976b
Darinmcc <50928612+Darinmcc@users.noreply.github.com>
2019-06-24 17:34:44 -0400

    added aggressive and conservative logic

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -39,7 +39,7 @@ request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&sym
 
 response = requests.get(request_url) #< response variable - sends get requests, specify the URL for the request - see documentation
 risk_options = ["cautious", "balanced", "aggressive"]
-risk_profile = input("Please enter your risk tolerance: 'cautious', 'balanced', 'aggressive'      ")
+risk_profile = input("Please enter your risk tolerance: 'cautious', 'balanced', 'aggressive' \n")
 if risk_profile not in risk_options:
     print("Risk profile should be: 'cautious', 'balanced', or 'aggressive'. Please try again")
     exit()
@@ -89,9 +89,10 @@ closing_prices = []
 
 for date in dates:
     closing_price = tsd[date]["4. close"]
-    closing_prices.append(float(close_price))
-mean = statistics.mean(closing_prices) #> 4.666666666666667
-mode = statistics.median(closing_prices) #> 4
+    closing_prices.append(float(closing_price))
+
+
+ 
 
 
 #max_high = [high for high in parsed_response["Time Series (Daily)"][f"{last_tradedate}"]["2. high"] ]
@@ -100,8 +101,11 @@ latest_close_usd = to_usd(float(latest_close))
 recent_high_usd = to_usd(float(recent_high))
 recent_low_usd = to_usd(float(recent_low))
 
-
-
+#stock statistics
+mean = float(statistics.mean(closing_prices)) #> 4.666666666666667
+mode = float(statistics.median(closing_prices)) #> 4
+vol =  (float(recent_high) - float(recent_low)) / float(latest_close)
+float_latest_close = float(latest_close)
 
 csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
  #don't change csv file path or __file__ variable
@@ -127,6 +131,30 @@ with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writin
 
 print(mean)
 print(mode)
+print(vol)
+
+if risk_profile == "aggressive":
+    if float_latest_close < mean and vol > .2:
+        recommendation = "BUY!"
+        recommendation_reason = f"Price below average close: {to_usd(mean)} and volatility is high"
+    elif float_latest_close < mean:
+        recommendation = "HOLD"
+        recommendation_reason = f"Price is below average close: {to_usd(mean)} , could take some time for price to recover"
+    else:
+        recommendation = "SELL!"
+        recommendation_reason = f"Price is at or above average close: {to_usd(mean)} , could be overvalued"
+
+if risk_profile == "conservative":
+    if float_latest_close > mean and vol < .1:
+        recommendation = "BUY!"
+        recommendation_reason = f"Price is above average close: {to_usd(mean)} and volatility is low"
+    elif float_latest_close < mean and vol < .1:
+        recommendation = "HOLD"
+        recommendation_reason = f"Price is below average close: {to_usd(mean)}, price could be on a downswing and volatility is low"
+    else:
+        recommendation = "SELL!"
+        recommendation_reason = "Volatility is higher than risk profile would warrant"
+
 
 print("-------------------------")
 print(f"SELECTED SYMBOL: {uppersymbol}")
@@ -139,8 +167,8 @@ print(f"LATEST CLOSE: {latest_close_usd}")
 print(f"RECENT HIGH: {recent_high_usd}")
 print(f"RECENT LOW: {recent_low_usd}")
 print("-------------------------")
-print("RECOMMENDATION: BUY!")
-print("RECOMMENDATION REASON: TODO")
+print(f"RECOMMENDATION: {recommendation}")
+print(f"RECOMMENDATION REASON: {recommendation_reason}")
 print("-------------------------")
 print(f"WRITING DATA TO CSV: {csv_file_path}")
 print("-------------------------")
